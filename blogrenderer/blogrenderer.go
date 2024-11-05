8a4a6457ce3650d29dcdc3aaa2252a10d2ebf7fb
Saga-sanga <vulcan248@gmail.com>
2024-11-05 16:05:49 +0530

    feat: add feat to convert md to html and passing tests

--- blogrenderer/blogrenderer.go
+++ blogrenderer/blogrenderer.go
@@ -1,11 +1,15 @@
 package blogrenderer
 
 import (
+	"bytes"
 	"embed"
 	"html/template"
 	"io"
 
 	"github.com/Saga-sanga/blogposts"
+	"github.com/yuin/goldmark"
+	"github.com/yuin/goldmark/extension"
+	"github.com/yuin/goldmark/renderer/html"
 )
 
 var (
@@ -13,13 +17,49 @@ var (
 	postTemplates embed.FS
 )
 
-func Render(w io.Writer, p blogposts.Post) error {
+type PostRenderer struct {
+	templ    *template.Template
+	markdown goldmark.Markdown
+}
+
+func NewPostRenderer() (*PostRenderer, error) {
 	templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
 	if err != nil {
+		return nil, err
+	}
+
+	markdown := goldmark.New(
+		goldmark.WithExtensions(extension.GFM),
+		goldmark.WithRendererOptions(html.WithHardWraps()),
+	)
+
+	return &PostRenderer{templ: templ, markdown: markdown}, nil
+}
+
+func (r *PostRenderer) Render(w io.Writer, p blogposts.Post) error {
+	var buf bytes.Buffer
+
+	if err := r.ConvertToMarkdown(&buf, p.Body); err != nil {
 		return err
 	}
 
-	if err := templ.ExecuteTemplate(w, "blog.gohtml", p); err != nil {
+	data := struct {
+		blogposts.Post
+		HTMLBody template.HTML
+	}{
+		Post:     p,
+		HTMLBody: template.HTML(buf.String()),
+	}
+
+	if err := r.templ.ExecuteTemplate(w, "blog.gohtml", data); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *PostRenderer) ConvertToMarkdown(w io.Writer, mdContent string) error {
+	if err := r.markdown.Convert([]byte(mdContent), w); err != nil {
 		return err
 	}
 
--- blogrenderer/blogrenderer_test.go
+++ blogrenderer/blogrenderer_test.go
@@ -2,6 +2,7 @@ package blogrenderer_test
 
 import (
 	"bytes"
+	"io"
 	"testing"
 
 	"github.com/Saga-sanga/blogposts"
@@ -19,13 +20,51 @@ func TestRender(t *testing.T) {
 		}
 	)
 
+	postRenderer, err := blogrenderer.NewPostRenderer()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	t.Run("it converts a single post to HTML", func(t *testing.T) {
 		buf := bytes.Buffer{}
 
-		if err := blogrenderer.Render(&buf, aPost); err != nil {
+		if err := postRenderer.Render(&buf, aPost); err != nil {
 			t.Fatal(err)
 		}
 
 		approvals.VerifyString(t, buf.String())
 	})
+
+	t.Run("it converts md body to HTML", func(t *testing.T) {
+		var buf bytes.Buffer
+
+		if err := postRenderer.ConvertToMarkdown(&buf, aPost.Body); err != nil {
+			t.Fatal(err)
+		}
+
+		approvals.VerifyString(t, buf.String())
+	})
+}
+
+func BenchmarkRender(b *testing.B) {
+	var (
+		aPost = blogposts.Post{
+			Title:       "hello world",
+			Body:        "This is a post",
+			Description: "This is a description",
+			Tags:        []string{"go", "tdd"},
+		}
+	)
+
+	postRenderer, err := blogrenderer.NewPostRenderer()
+
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		postRenderer.Render(io.Discard, aPost)
+	}
 }
